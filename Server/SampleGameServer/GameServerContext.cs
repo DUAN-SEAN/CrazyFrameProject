c8894a1797e581e563d1f05e7d19f1d2c8284178
段 瑞 <dr18627820572@live.com>
2019-05-16 17:42:56 +0800

    dr

--- Server/SampleGameServer/GameServerContext.cs
+++ Server/SampleGameServer/GameServerContext.cs
@@ -7,6 +7,9 @@ using Crazy.NetSharp;
 using Crazy.ServerBase;
 namespace GameServer
 {
+    /// <summary>
+    /// 玩家现场
+    /// </summary>
     public sealed class GameServerContext:PlayerContextBase
     {
         public override Task OnMessage(ILocalMessage msg)
