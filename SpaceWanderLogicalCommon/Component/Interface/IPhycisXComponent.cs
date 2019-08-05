0798071b325b50923dcd6d1b7019061cf54ba413
悠 吴 <673422559@qq.com>
2019-08-05 08:24:28 +0800

    1

--- SpaceWanderLogicalCommon/Component/Interface/IPhycisXComponent.cs
+++ SpaceWanderLogicalCommon/Component/Interface/IPhycisXComponent.cs
@@ -6,11 +6,22 @@ using System.Threading.Tasks;
 
 namespace SpaceWanderLogicalCommon
 {
+    /// <summary>
+    /// 对外接口
+    /// </summary>
     public interface IPhycisXComponent
     {
+
     }
 
     public interface IInternalPhycisXComponent:IPhycisXComponent
     {
+
+        /// <summary>
+        /// 得到当前位置
+        /// </summary>
+        void GetCurrentPosition();
+
+        void GetCurrentForward();
     }
 }
