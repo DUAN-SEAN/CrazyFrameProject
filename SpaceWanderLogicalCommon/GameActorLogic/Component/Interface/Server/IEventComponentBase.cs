aa1a61d2e33b8998147ae08dac5e6f172aa36fb7
悠 吴 <673422559@qq.com>
2019-08-05 09:32:15 +0800

    LevelActor

--- SpaceWanderLogicalCommon/GameActorLogic/Component/Interface/Server/IEventComponentBase.cs
+++ SpaceWanderLogicalCommon/GameActorLogic/Component/Interface/Server/IEventComponentBase.cs
@@ -13,7 +13,7 @@ namespace GameActorLogic
     public interface IEventComponentBase
     {
         /// <summary>
-        /// 待处理的事件集合
+        /// 待Actor处理的事件集合
         /// </summary>
         List<IEventMessage> HandleEventMessages
         {
@@ -21,32 +21,26 @@ namespace GameActorLogic
             set;
         }
         /// <summary>
-        /// 待转发的事件集合
+        /// 待Actor向外转发的事件集合
         /// </summary>
         List<IEventMessage> ForWardEventMessages
         {
             get;
             set;
         }
-        /// <summary>
-        /// 执行需要处理的事件消息
-        /// </summary>
-        void TickHandleEvent();
+     
         /// <summary>
         /// 添加要处理的集合
         /// </summary>
-        /// <param name="msg"></param>
         void AddHandleEventMessage(IEventMessage msg);
         /// <summary>
         /// 获取需要处理的事件，获取完后需要清空集合
         /// 在服务器上由服务器转发到客户端对应的相同组件上，在客户端上由相关组件获取。
         /// </summary>
-        /// <returns></returns>
         List<IEventMessage> GetForWardEventMessages();
         /// <summary>
         /// 向转发事件集合中添加要转发的消息
         /// </summary>
-        /// <param name="msg"></param>
         void AddForWardEventMessages(IEventMessage msg);
 
     }
@@ -55,6 +49,9 @@ namespace GameActorLogic
     /// </summary>
     public interface IEventInternalComponentBase : IEventComponentBase
     {
-
+        /// <summary>
+        /// 执行需要处理的事件消息
+        /// </summary>
+        void TickHandleEvent();
     }
 }
---
+++ SpaceWanderLogicalCommon/GameActorLogic/LevelActor/ILevelActorBaseContainer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameActorLogic
+{
+    /// <summary>
+    /// 关卡Actor 对外接口
+    /// </summary>
+    public interface ILevelActorBaseContainer:
+        IEventComponentBase
+    {
+        /// <summary>
+        /// 获得ID
+        /// </summary>
+        long GetLevelID();
+
+        
+
+        /// <summary>
+        /// 开启关卡
+        /// </summary>
+        void Start();
+
+        
+        /// <summary>
+        /// 清理关卡现场
+        /// </summary>
+        void Dispose();
+    }
+
+    /// <summary>
+    /// 关卡Actor 对内接口
+    /// </summary>
+    public interface ILevelActorComponentBaseContainer
+    {
+        /// <summary>
+        /// 获取事件组件
+        /// </summary>
+        IEventComponentBase GetEventComponentBase();
+    }
+}
