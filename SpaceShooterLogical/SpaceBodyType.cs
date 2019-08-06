929d0d8397afd453e6faadfe442eaad8c6d02171
悠 吴 <673422559@qq.com>
2019-08-06 10:13:07 +0800

    删掉了旧的内容

--- SpaceShooterLogical/SpaceBodyType.cs
+++ SpaceShooterLogical/SpaceBodyType.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SpaceShip.AI;
+
 namespace SpaceShip.Base
 {
     public class SpaceBodyType
