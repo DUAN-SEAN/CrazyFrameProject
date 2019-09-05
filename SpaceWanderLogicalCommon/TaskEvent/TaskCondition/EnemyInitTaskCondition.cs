6deb4a52f19d86f66305a3e67c0c29cf460b1ca8
悠 吴 <673422559@qq.com>
2019-09-05 17:15:36 +0800

    1

--- SpaceWanderLogicalCommon/TaskEvent/TaskCondition/EnemyInitTaskCondition.cs
+++ SpaceWanderLogicalCommon/TaskEvent/TaskCondition/EnemyInitTaskCondition.cs
@@ -43,7 +43,7 @@ namespace GameActorLogic
                     for(int i = 0; i < item.Value; i++)
                     {
                         levelActor.AddEventMessagesToHandlerForward(new InitEventMessage(actorid:levelActor.GetCreateInternalComponentBase().GetCreateID(),actortype: item.Key, camp: LevelActorBase.EnemyCamp, point_x: EnemyPoint[i % EnemyPoint.Length].X, point_y: EnemyPoint[i % EnemyPoint.Length].Y, angle: 0));
-                        Log.Trace("EnemyInitTaskCondition: StartCondition 任务id：" + taskEventBase.GetTaskId() + " 生成第" + i + "个对象 key:" + item.Key);
+                        //Log.Trace("EnemyInitTaskCondition: StartCondition 任务id：" + taskEventBase.GetTaskId() + " 生成第" + i + "个对象 key:" + item.Key);
                     }
                 }
             }
