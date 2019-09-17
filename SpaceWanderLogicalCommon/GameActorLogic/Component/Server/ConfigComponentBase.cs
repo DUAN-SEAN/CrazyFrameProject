8eb3054849b582e9ef34d65bddbecdc0bba26d0f
悠 吴 <673422559@qq.com>
2019-09-17 16:04:29 +0800

    1

--- SpaceWanderLogicalCommon/GameActorLogic/Component/Server/ConfigComponentBase.cs
+++ SpaceWanderLogicalCommon/GameActorLogic/Component/Server/ConfigComponentBase.cs
@@ -287,7 +287,7 @@ namespace GameActorLogic
 
             envirActor = new EnvirActor(0, ActorTypeBaseDefine.BlackHole, level);
             envirActor.CreateInitData(new InitData());
-            envirActor.CreateAiComponent(new BlackHoleAiComponent(envirActor, 50, 10));
+            envirActor.CreateAiComponent(new BlackHoleAiComponent(envirActor, 50, 1));
             ConfigActors.Add(ActorTypeBaseDefine.BlackHole, envirActor);
 
             #endregion
